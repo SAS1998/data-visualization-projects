a520baafca8a4705849e3ea5861a92f50842ed2c
yaylinda <lindazheng1993@gmail.com>
2017-07-14 00:19:52 -0500

    chose some colors

--- HarryPotterCharacterScreentime/code.R
+++ HarryPotterCharacterScreentime/code.R
@@ -1,4 +1,5 @@
 library(ggplot2)
+library(RColorBrewer)
 setwd("~/Developer/data-visualization-projects/HarryPotterCharacterScreentime")
 
 movie_order = as.factor(c("Philosopher's/Sorcerer's Stone", 
@@ -25,20 +26,33 @@ data$movie = factor(data$movie, levels=movie_order)
 data = data[complete.cases(data), ]
 # data$time_percent = (data$time/movie_minutes[data$movie])*100
 
+mean(data$time)
+median(data$time)
+
+cols = c("#1B9E77", "#D95F02", "#E7298A", "#66A61E", "#E6AB02", "#A6761D", "#666666", "#666666")
+
 plot = ggplot(data=data, aes(x=character, y=time, fill=movie)) +
   geom_col(position = position_stack(reverse = TRUE)) + 
   ggtitle("Screen Time of Harry Potter Characters") +
   xlab("Character") + ylab("Screen Time (minutes)") +
   guides(colour = guide_legend(reverse=T)) + 
-  scale_fill_discrete(name="Movie") +
   scale_y_continuous(breaks=seq(0,500,by=100), labels=seq(0,500,by=100)) +
+  scale_fill_manual(name="Movie", values=cols) +
   theme(
-    panel.grid.minor.x=element_blank()
+    panel.grid.minor.x=element_blank(),
+    aspect.ratio=1,
+    text=element_text(size=12), 
+    plot.title=element_text(size=16),
+    legend.text=element_text(size=12),
+    legend.key.size = unit(1, "lines"),
+    axis.text=element_text(size=12)
   ) +
   coord_flip()
 plot
 
-png(paste("plot_", k, ".png", sep=""), width=1000, height=1000, units="px") 
+ggsave(paste("plot_", k, "_dark.png", sep=""), dpi=500)
 
 
+display.brewer.all()
+brewer.pal(8, "Dark2")
 
