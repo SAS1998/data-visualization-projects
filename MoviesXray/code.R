56f1c606ead7f56919809f0e81fb27681d6cd7f8
yaylinda <lindazheng1993@gmail.com>
2017-07-28 17:06:26 -0500

    refactored code to do loop

--- MoviesXray/code.R
+++ MoviesXray/code.R
@@ -1,29 +1,38 @@
 setwd("~/Developer/data-visualization-projects/MoviesXray")
 
 library(ggplot2)
-library(ggExtra)
 
-# clean up data
-data = read.csv("xray.csv")
-data$start = data$start/1000
-data$end = data$end/1000
-charactersOrder = as.character(unique(data[with(data, order(start)), 2]))
-data$character = factor(data$character, levels=rev(charactersOrder))
+files = list.files(path = "data/")
 
-# plots
-g1 = ggplot(data=data) +
-  geom_segment(aes(x=character, xend=character, y=start, yend=end, colour=character, size=5)) +
-  scale_y_discrete(limits=seq(0,7766,600), labels=seq(0,7766,600)) + 
-  labs(
-    title="The Matrix Characters On-Screen Time",
-    y="Time (s)", 
-    x="Characters (in order of appearance)"
-  ) +
-  theme(legend.position="none") +
-  coord_flip()
-g1
-ggsave("plots/thematrixcharacters.png",height=5,width=7,dpi=500)
-
+for (i in 1:length(files)) {
+  filename = files[i]
+  movie_name = unlist(strsplit(filename, "-data.csv")[1])
+  movie_name_title
+  
+  data = read.csv(paste0("data/", files[i]))
+  
+  # clean up data
+  data$start = data$start/1000
+  data$end = data$end/1000
+  charactersOrder = as.character(unique(data[with(data, order(start)), 2]))
+  data$character = factor(data$character, levels=rev(charactersOrder))
+  
+  max_time = max(data$end)
+  
+  # plot
+  g1 = ggplot(data=data) +
+    geom_segment(aes(x=character, xend=character, y=start, yend=end, colour=character, size=5)) +
+    scale_y_discrete(limits=seq(0,max_time,600), labels=seq(0,max_time,600)) + 
+    labs(
+      title=paste0(movie_name_title, " Characters On-Screen Time"),
+      y="Time (s)", 
+      x="Characters (in order of appearance)"
+    ) +
+    theme(legend.position="none") +
+    coord_flip()
+  g1
+  ggsave(paste0("plots/", movie_name, "-characters.png"), height=5, width=7, dpi=500)
+}
 
 ############################################################
 
--- MoviesXray/read-xray.py
+++ MoviesXray/read-xray.py
@@ -3,29 +3,33 @@ import re
 import csv
 import sys
 
-filename = sys.argv[1]
+import os
 
-moviename = filename.split("-xray.json")[0]
-resultfilename = moviename + "-data.csv"
+for filename in os.listdir(os.getcwd() + "/xray/"):
 
-with open(filename) as data:
-    data = json.load(data)
+    print "processing " + filename
 
-scenes = data['page']['sections']['left']['widgets']['widgetList']
-scenes = scenes[0]['widgets']['widgetList'][1]['partitionedChangeList']
+    moviename = filename.split("-xray.json")[0]
+    resultfilename = moviename + "-data.csv"
 
-with open(resultfilename, 'wb') as out:
-    w = csv.writer(out)
-    w.writerow(['nconst', 'character', 'start', 'end'])
-    for s in scenes:
-        start = s['timeRange']['startTime']
-        end = s['timeRange']['endTime']
-        for init in s['initialItemIds']:
-            rd = re.search('/name/(nm.+)/(.+)', init)
-            if rd is not None:
-                w.writerow([rd.group(1), rd.group(2), start, end])
-        for item in s['changesCollection']:
-            rd = re.search('/name/(nm.+)/(.+)', item['itemId'])
-            if rd is not None:
-                iStart = item['timePosition']
-                w.writerow([rd.group(1), rd.group(2), iStart, end])
+    with open("xray/" + filename) as data:
+        data = json.load(data)
+
+    scenes = data['page']['sections']['left']['widgets']['widgetList']
+    scenes = scenes[0]['widgets']['widgetList'][1]['partitionedChangeList']
+
+    with open("data/" + resultfilename, 'wb') as out:
+        w = csv.writer(out)
+        w.writerow(['character', 'start', 'end'])
+        for s in scenes:
+            start = s['timeRange']['startTime']
+            end = s['timeRange']['endTime']
+            for init in s['initialItemIds']:
+                rd = re.search('/name/(nm.+)/(.+)', init)
+                if rd is not None:
+                    w.writerow([rd.group(2), start, end])
+            for item in s['changesCollection']:
+                rd = re.search('/name/(nm.+)/(.+)', item['itemId'])
+                if rd is not None:
+                    iStart = item['timePosition']
+                    w.writerow([rd.group(2), iStart, end])
