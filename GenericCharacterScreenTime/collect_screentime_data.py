a51aea1a7ca8ba8fb096a74cb02d2e499d5c8686
yaylinda <lindazheng1993@gmail.com>
2017-07-30 16:25:15 -0500

    wrote python code to get screen time for shows in metadata file

--- GenericCharacterScreenTime/collect_screentime_data.py
+++ GenericCharacterScreenTime/collect_screentime_data.py
@@ -1,15 +1,40 @@
 import os
 import csv
 
-def get_html():
-	print "\nGetting html for movies table..."	
-	url_string = "http://www.imdb.com/list/ls064205706/"
-	os.system("curl " + url_string + " > hunger_games_onscreen_time.txt")
+def read_metadata():
+	metafile = open("imdb_urls.txt")
+	content = metafile.readlines()
+	content = [x.strip() for x in content]
 
-def get_data_from_html():
-	print "\nParsing html file..."
+	title_line = -1
+	url_line = -1
 
-	file = open("hunger_games_onscreen_time.txt")
+	for i in range(0, len(content)):
+		line = content[i]
+		if (line.startswith("[Titles]")):
+			title_line = i
+		elif (line.startswith("[URLS]")):
+			url_line = i
+
+	titles = []
+	urls = []
+
+	titles = content[title_line+1:url_line]
+	urls = content[url_line+1:len(content)]
+
+	print "titles: " + str(titles)
+	print "urls: " + str(urls)
+
+	return (titles, urls)
+
+def get_html(url, title):
+	print "\nGetting html for [" + title + "]..."	
+	os.system("curl " + url + " > temp.txt")
+
+def get_data_from_html(title):
+	print "\nParsing html for [" + title + "]..."
+
+	file = open("temp.txt")
 	content = file.readlines()
 	content = [x.strip() for x in content]
 	
@@ -25,52 +50,60 @@ def get_data_from_html():
 		elif line.startswith(SCREENTIME_INFO_LINE_START):
 			character_desc_map[character] = line
 
-	print character_desc_map
+	# print character_desc_map
 
 	data = []
 	for character in character_desc_map.keys():
 		desc = character_desc_map[character]
 		# print desc
-		for movie in movie_titles:
-			movie_time_min = 0
-			# print movie
-			if movie in desc:
-				time_string = desc.split(movie)[1].split(" &lt;")[1].split("&gt")[0]
-				movie_time_split = time_string.split(":")
-				if ("x" not in movie_time_split and "15<br/>* ...Deathly Hallows, Part I" not in movie_time_split):
-					if len(movie_time_split) == 1:
-						movie_time_min = int(movie_time_split[0])
-					elif len(movie_time_split) == 2:
-						if (movie_time_split[0] == ""):
-							movie_time_min = int(movie_time_split[1]) / 60.0
-						else:
-							movie_time_min += int(movie_time_split[0])
-							movie_time_min += int(movie_time_split[1]) / 60.0
+
+		onscreen_time_min = 0
+
+		time_string = desc.split(" minutes<span ")[0]
+		time_string = time_string[len(time_string)-6:]
+		if (time_string.find(">") > -1):
+			time_string = time_string.split(">")[1]
+		
+		onscreen_time_split = time_string.split(":")
+		if ("x" not in onscreen_time_split):
+			if len(onscreen_time_split) == 1:
+				onscreen_time_min = int(onscreen_time_split[0])
+			elif len(onscreen_time_split) == 2:
+				if (onscreen_time_split[0] == ""):
+					onscreen_time_min = int(onscreen_time_split[1]) / 60.0
+				else:
+					onscreen_time_min += int(onscreen_time_split[0])
+					onscreen_time_min += int(onscreen_time_split[1]) / 60.0
 
 			datum = {}
 			datum["character"] = character
-			datum["title"] = movie
-			datum["time"] = movie_time_min
+			datum["title"] = title
+			datum["time"] = onscreen_time_min
 			data.append(datum)
 
 	file.close()
 	return data
 
-def write_to_csv(data):
-	print "\nWriting to csv file..."
-	csv_file = open('screentime.csv', 'w')
+def write_to_csv(data, title):
+	print "\nWriting to csv file for [" + title + "]..."
+	csv_file = open(title + "_screentime.csv", "w")
 	csv_writer = csv.writer(csv_file, delimiter=',')
-	csv_writer.writerow(['character', 'movie', 'time'])
+	csv_writer.writerow(['character', 'title', 'time'])
 	
 	for datum in data:
-		csv_writer.writerow([datum['character'], datum['movie'], datum['time']])
+		csv_writer.writerow([datum['character'], datum['title'], datum['time']])
 
 	csv_file.close()
 
 def main():
-	get_html()
-	data = get_data_from_html()
-	write_to_csv(data)
-	print "\nDone!"
+	(titles, urls) = read_metadata()
+
+	for i in range(0, len(titles)):
+		title = titles[i]
+		url = urls[i]
+		get_html(url, title)
+		data = get_data_from_html(title)
+		write_to_csv(data, title)
+		print "\nDone!"
 
 main()
--- GenericCharacterScreenTime/stranger_things.py
+++
@@ -1,3 +0,0 @@
-imdb_url="http://www.imdb.com/list/ls062432534/"
-html_temp_file="stranger_things_onscreen_time.txt"
-output_csv_file="stranger_things_screentime.csv"
